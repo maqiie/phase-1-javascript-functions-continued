130c51b62b5fc6f63ef25398b3495b909d8b2865
maqiie <candiemp2580@gmai.com>
2022-11-27 14:59:18 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+function mondayWork (activity = 'go to the office') {
+    return (`This Monday, I will ${activity}.`);
+}
+mondayWork();
+
+
+function wrapAdjective(wrapper = "*") {
+    function message(recognition = "special") {
+      return `You are ${wrapper}${recognition}${wrapper}!`;
+    };
+    return message;
+};
+
+
+wrapAdjective("||");
+result("high performance employee");
